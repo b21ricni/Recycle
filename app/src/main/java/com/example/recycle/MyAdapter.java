3db6cbcd86a936a89ed5f964edf2ca38261b46ec
b21ricni <b21ricni@student.his.se>
2022-04-06 14:51:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/recycle/MyAdapter.java
@@ -0,0 +1,33 @@
+package com.example.recycle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    List<String> mountains = Arrays.asList("Kinnekulle", "Billingen","Mount Everest","Kebnekaise","Fiji" );
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mountain, parent);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(mountains.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountains.size();
+    }
+}
