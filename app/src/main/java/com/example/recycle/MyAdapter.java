f3d3128cfa2642c4f534e78cd0eb6e9cde7c2e84
b21ricni <b21ricni@student.his.se>
2022-04-06 15:03:15 +0200

    IT WORKS

--- app/src/main/java/com/example/recycle/MyAdapter.java
+++ app/src/main/java/com/example/recycle/MyAdapter.java
@@ -17,7 +17,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mountain, parent);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mountain, parent, false);
         return new MyViewHolder(view);
     }
 
